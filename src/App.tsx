f6fae713d7f21f39ccd3ac3a5b186859ecef9fa3
l11040 <113540683+l11040@users.noreply.github.com>
2024-04-25 15:42:40 +0900

    Init : tailwindcss 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import './App.css';
+
 function App() {
   return (
-    <></>
+    <>
+      <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+        l11040's Blog
+      </h1>
+    </>
   );
 }
 
