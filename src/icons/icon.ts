f2a75f3d746babc6d6e936fabc3e9fd9ced6f912
l11040 <113540683+l11040@users.noreply.github.com>
2024-04-26 16:47:28 +0900

    Update : 아이콘 따로분리 후 호출로 변경

---
+++ src/icons/icon.ts
@@ -0,0 +1,11 @@
+import { ReactComponent as fire } from "./fire.svg";
+import { ReactComponent as book } from "./book.svg";
+import { ReactComponent as people } from "./people.svg";
+import { ReactComponent as edit } from "./edit.svg";
+import { ReactComponent as atom } from "./atom.svg";
+
+export const Fire = fire;
+export const Book = book;
+export const People = people;
+export const Edit = edit;
+export const Atom = atom;
