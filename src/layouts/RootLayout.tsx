94771534ebf83a6132c05c722369a645f29d411b
l11040 <113540683+l11040@users.noreply.github.com>
2024-04-28 22:49:02 +0900

    Fix : 스타일 오류 수정

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <>
       <div className="max-w-screen-xl xl:lg:md:sm:mx-auto md:pt-8">
-        <div className="flex flex-col md:flex-row gap-4 justify-between mx-6 my-8 h-auto">
+        <div className="flex flex-col md:flex-row gap-4 justify-between px-6 py-8 h-auto">
           <div className="w-full md:w-auto ">
             <div className="md:sticky top-10 flex flex-col gap-4 max-w-[400px] mx-auto">
               <Profile
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: LayoutProps) {
               <SideMenu />
             </div>
           </div>
-          <div className="w-full overflow-x-hidden">
+          <div className="w-full">
             <div>{children}</div>
           </div>
         </div>
