157db5de0a41c33706bd436e5a25e6aec4347afa
l11040 <113540683+l11040@users.noreply.github.com>
2024-04-29 14:20:10 +0900

    Add : Object 아이콘추가

--- src/icons/icon.ts
+++ src/icons/icon.ts
@@ -4,6 +4,7 @@ import { ReactComponent as people } from "./people.svg";
 import { ReactComponent as edit } from "./edit.svg";
 import { ReactComponent as atom } from "./atom.svg";
 import { ReactComponent as books } from "./books.svg";
+import { ReactComponent as objects } from "./objects.svg";
 
 export const Fire = fire;
 export const Book = book;
@@ -11,3 +12,4 @@ export const People = people;
 export const Edit = edit;
 export const Atom = atom;
 export const Books = books;
+export const Objects = objects;
