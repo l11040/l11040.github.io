a2b3ffa5642763c42ad395d23162695259d10409
l11040 <113540683+l11040@users.noreply.github.com>
2024-04-29 23:15:34 +0900

    Add : blog, post 라우터 등록

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import "../styles/App.css";
+import "../styles/app.css";
 import Home from "./Home";
+import RootLayout from "../layouts/RootLayout";
+import Blog from "./Blog";
+import Post from "./Blog/post";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <RootLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:pageId" element={<Post />} />
+      </Routes>
+    </RootLayout>
   );
 }
