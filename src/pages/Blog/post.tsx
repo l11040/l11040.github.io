96418cadc7d6c8302df56f8c23a443329de13e0d
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-02 21:43:29 +0900

    Add : 글 스켈레톤 UI추가

--- src/pages/Blog/post.tsx
+++ src/pages/Blog/post.tsx
@@ -2,10 +2,11 @@ import { useParams } from "react-router-dom";
 
 import useGetNotionPage from "../../api/useGetNotionPage";
 import NotionPost from "../../components/NotionPost";
+import SkeletonPost from "../../components/SkeletonPost";
 
 export default function Post() {
   const { pageId } = useParams();
-  const { data: post } = useGetNotionPage(pageId);
+  const { data: post, isLoading, isFetching } = useGetNotionPage(pageId);
 
-  return <NotionPost blockMap={post} />;
+  return <>{isLoading ? <SkeletonPost /> : <NotionPost blockMap={post} />}</>;
 }
